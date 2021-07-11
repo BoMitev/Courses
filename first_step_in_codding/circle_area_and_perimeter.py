0c8bfcb1e42b14cd91549b61f7eabe79a4ee5ccc
Tarantulata <80598585+Tarantulata@users.noreply.github.com>
2021-07-11 11:36:40 +0300

    Delete first_step_in_codding directory

--- first_step_in_codding/circle_area_and_perimeter.py
+++
@@ -1,7 +0,0 @@
-import math
-radius = float(input())
-
-circle_area = math.pi * radius * radius
-circle_perimeter = 2 * math.pi * radius
-
-print(f'{circle_area:.2f}\n{circle_perimeter:.2f}')
--- first_step_in_codding/vegetable_market.py
+++
@@ -1,9 +0,0 @@
-vegetable_price = float(input())
-fruit_price = float(input())
-vegetable_kilos = int(input())
-fruit_kilos = int(input())
-
-vegetable_profit = (vegetable_price * vegetable_kilos) / 1.94
-fruit_profit = (fruit_price * fruit_kilos) / 1.94
-profit = vegetable_profit + fruit_profit
-print(f'{profit:.2f}')
