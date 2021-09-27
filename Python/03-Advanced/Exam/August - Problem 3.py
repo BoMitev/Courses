f498317e2dc08f25b285314475df2433e116179c
Bo.Mitev <80598585+BoMitev@users.noreply.github.com>
2021-09-27 22:52:27 +0300

    commit

--- Python/03-Advanced/Exam/August - Problem 3.py
+++ Python/03-Advanced/Exam/August - Problem 3.py
@@ -1,12 +1,9 @@
-# Write a function called numbers_searching which receives a different amount of parameters. All parameters will be integer numbers forming a sequence of consecutive numbers. 
-# Your task is to find an unknown amount of duplicates from the given sequence and a missing value, such that all the duplicate values and the missing value are between the 
-# smallest and the biggest received number.
+# Write a function called numbers_searching which receives a different amount of parameters. All parameters will be integer numbers forming a sequence of consecutive numbers. Your task is to find an unknown amount of duplicates from the given sequence and a missing value, such that all the duplicate values and the missing value are between the smallest and the biggest received number.
 # The function should return a list with the last missing number as a first argument and a sorted list, containing the duplicates found, in ascending order.
 # For example: if we have the following numbers: 1, 2, 4, 2, 5, 4 will return 3 as missing number and 2, 4 as duplicate numbers in the following format: [3, [2, 4]]
 
+
 # First solving
-
-
 def numbers_searching(*args, missing_n=None, res=None):
     if not args:
         res.sort()
@@ -22,7 +19,6 @@ def numbers_searching(*args, missing_n=None, res=None):
         res.append(args[0])
 
     return numbers_searching(*args[1:], missing_n=missing_n, res=res)
-
 
 # Second solving
 # def numbers_searching(*nums):
