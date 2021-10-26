355827babe4bd3b76d35ef0ed5db17191e4fdca1
Boris Mitev <80598585+BoMitev@users.noreply.github.com>
2021-10-26 23:20:00 +0300

    commit OOP

---
+++ SoftUni/Python/04-OOP/2.Classes_and_objects/time.py
@@ -0,0 +1,41 @@
+class Time:
+    max_hours = 23
+    max_minutes = 59
+    max_seconds = 59
+
+    def __init__(self, hours: int, minutes: int, seconds: int):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def set_time(self, hours: int, minutes: int, seconds: int):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def get_time(self):
+        clock = "{:02d}:".format(self.hours)+"{:02d}:".format(self.minutes)+"{:02d}".format(self.seconds)
+        return clock
+
+    def next_second(self):
+        self.seconds += 1
+        if self.seconds > Time.max_seconds:
+            self.seconds = 0
+            self.minutes += 1
+        if self.minutes > Time.max_minutes:
+            self.minutes = 0
+            self.hours += 1
+        if self.hours > Time.max_hours:
+            self.hours = 0
+
+        return Time.get_time(self)
+
+
+time = Time(9, 30, 59)
+print(time.next_second())
+
+time = Time(10, 59, 59)
+print(time.next_second())
+
+time = Time(23, 59, 59)
+print(time.next_second())
