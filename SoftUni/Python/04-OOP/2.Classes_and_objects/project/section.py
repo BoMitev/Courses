09acc15f5c62c1d895c9f4b3e91e223cab18d5b1
Boris Mitev <80598585+BoMitev@users.noreply.github.com>
2021-10-27 23:10:40 +0300

    Update section.py

--- SoftUni/Python/04-OOP/2.Classes_and_objects/project/section.py
+++ SoftUni/Python/04-OOP/2.Classes_and_objects/project/section.py
@@ -1,11 +1,9 @@
-from task import Task
-
 class Section:
     def __init__(self, name: str):
         self.name = name
         self.tasks = []
 
-    def add_task(self, new_task: Task):
+    def add_task(self, new_task):
         if new_task in self.tasks:
             return f"Task is already in the section {self.name}"
         self.tasks.append(new_task)
@@ -29,18 +27,3 @@ class Section:
     def view_section(self):
         nl = '\n'
         return f'Section {self.name}:\n{nl.join([self.tasks[i].details() for i in range(len(self.tasks))])}'
-
-
-task = Task("Make bed", "27/05/2020")
-print(task.change_name("Go to University"))
-print(task.change_due_date("28.05.2020"))
-task.add_comment("Don't forget laptop")
-print(task.edit_comment(0, "Don't forget laptop and notebook"))
-print(task.details())
-section = Section("Daily tasks")
-print(section.add_task(task))
-second_task = Task("Make bed", "27/05/2020")
-section.add_task(second_task)
-#section.complete_task('Go to University')
-print(section.clean_section())
-print(section.view_section())
