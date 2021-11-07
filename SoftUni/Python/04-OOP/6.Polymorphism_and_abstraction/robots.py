ea2742d12217e58eae8d22db7ec838c09c251056
Boris Mitev <80598585+BoMitev@users.noreply.github.com>
2021-11-07 23:07:21 +0200

    commit

---
+++ SoftUni/Python/04-OOP/6.Polymorphism_and_abstraction/robots.py
@@ -0,0 +1,30 @@
+class Robot:
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def sensors_amount():
+        return 1
+
+
+class MedicalRobot(Robot):
+    @staticmethod
+    def sensors_amount():
+        return 6
+
+
+class ChefRobot(Robot):
+    @staticmethod
+    def sensors_amount():
+        return 4
+
+
+class WarRobot(Robot):
+    @staticmethod
+    def sensors_amount():
+        return 12
+
+
+def number_of_robot_sensors(robot):
+    print(robot.sensors_amount())
+
