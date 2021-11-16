5e9eb2c9cac18168d9663c90c3452211001becc1
Boris Mitev <80598585+BoMitev@users.noreply.github.com>
2021-11-16 21:54:11 +0200

    data structures

---
+++ Udemy/Data_Structures_&_Algoritms/Data_structures/Trees.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    # Insert Method
+    def insert(self, value):
+        new_node = Node(value)
+
+        if self.root is None:
+            self.root = new_node
+            return True
+
+        temp = self.root
+        while True:
+            if new_node.value == temp.value:
+                return False
+
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    # Contain Method
+    def contains(self, value):
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+
+        return False
