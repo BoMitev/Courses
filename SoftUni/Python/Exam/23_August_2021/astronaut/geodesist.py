a724b8d2b4df38b7f6af97d41d761ca209646f63
Boris Mitev <80598585+BoMitev@users.noreply.github.com>
2021-11-24 23:57:35 +0200

    commit

---
+++ SoftUni/Python/Exam/23_August_2021/astronaut/geodesist.py
@@ -0,0 +1,11 @@
+from everland.astronaut.astronaut import Astronaut
+
+
+class Geodesist(Astronaut):
+    DEFAULT_OXYGEN_LEVEL = 50
+
+    def __init__(self, name: str) -> None:
+        super().__init__(name, self.DEFAULT_OXYGEN_LEVEL)
+
+    def breathe(self) -> None:
+        self.oxygen -= self.BREATH_UNITS
